1b3e7972ec3003da3d47aacdbc86e11ec9627e73
michael spengler <43786652+michael-spengler@users.noreply.github.com>
2022-01-25 03:04:24 +0100

    Update is-prime.spec.ts

--- src/is-prime.spec.ts
+++ src/is-prime.spec.ts
@@ -1,10 +1,11 @@
 import { assertEquals } from "https://deno.land/std@0.86.0/testing/asserts.ts"
 import { isPrime } from "./is-prime.ts"
 
-Deno.test("should return true", async () => {
+Deno.test("should return true for 17", async () => {
     assertEquals(isPrime(17), true)
 })
 
-Deno.test("should return false", async () => {
+Deno.test("should return false for 20 and for 4", async () => {
     assertEquals(isPrime(20), false)
+    assertEquals(isPrime(4), false)
 })
