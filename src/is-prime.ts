e7fc3a6cda9faaf441471938ca54916c113ba5dd
michael spengler <43786652+michael-spengler@users.noreply.github.com>
2022-01-25 03:03:11 +0100

    Update is-prime.ts

--- src/is-prime.ts
+++ src/is-prime.ts
@@ -1,11 +1,14 @@
-export function isPrime(number: number) : boolean {
-    if (number <= 1) {
-        return false
+
+export function isPrime(n: number) {
+
+    let checkUntil = Math.floor((n / 2))
+    let counter = 2
+
+    while (counter <= checkUntil) {
+        if (n % counter === 0) return false
+
+        counter++
     }
-    for (let i=2; i<Math.ceil(number**0.5); i++) {
-        if (number % i == 0) {
-            return false
-        }
-    }
+
     return true
 }
