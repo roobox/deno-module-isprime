cb2ed94bb461719534767e8941cbfbf31945ee46
Marco Zeulner <marco-zeulner@gmx.de>
2022-01-26 09:50:51 +0100

    fixed bug and added tests

--- src/is-prime.spec.ts
+++ src/is-prime.spec.ts
@@ -1,10 +1,25 @@
 import { assertEquals } from "https://deno.land/std@0.86.0/testing/asserts.ts"
 import { isPrime } from "./is-prime.ts"
-
-Deno.test("should return true", async () => {
+// True tests
+Deno.test("should return true for 2", async () => {
+    assertEquals(isPrime(2), true)
+})
+Deno.test("should return true for 3", async () => {
+    assertEquals(isPrime(3), true)
+})
+Deno.test("should return true for 17", async () => {
     assertEquals(isPrime(17), true)
 })
-
-Deno.test("should return false", async () => {
-    assertEquals(isPrime(20), false)
+Deno.test("should return true for 97", async () => {
+    assertEquals(isPrime(97), true)
+})
+// False tests
+Deno.test("should return false for 1", async () => {
+    assertEquals(isPrime(1), false)
+})
+Deno.test("should return false for 4", async () => {
+    assertEquals(isPrime(4), false)
+})
+Deno.test("should return false for 120", async () => {
+    assertEquals(isPrime(120), false)
 })
--- src/is-prime.ts
+++ src/is-prime.ts
@@ -1,11 +1,14 @@
-export function isPrime(number: number) : boolean {
+export function isPrime(number: number): boolean {
     if (number <= 1) {
         return false
-    }
-    for (let i=2; i<Math.ceil(number**0.5); i++) {
-        if (number % i == 0) {
-            return false
+    } else if (number == 2) {
+        return true
+    } else {
+        for (let i = 2; i <= Math.ceil(number ** 0.5); i++) {
+            if (number % i == 0) {
+                return false
+            }
         }
+        return true
     }
-    return true
 }
